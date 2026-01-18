911b98cbb96a6050d895f572cc1086870ba18398
hamted <64844625+hamted@users.noreply.github.com>
2026-01-18 06:16:08 -0500

    Add header guards and update comments in gpio_qnx.h

--- gpio_qnx.h
+++ gpio_qnx.h
@@ -1,6 +1,10 @@
-// gpio_qnx.h
-#pragma once
+#ifndef GPIO_QNX_H
+#define GPIO_QNX_H
+
+// QNX GPIO wrapper using gpio-rp1
 int gpio_init_output(int pin);
 int gpio_init_input_pullup(int pin);
-int gpio_write(int pin, int value);   // value 0/1
-int gpio_read(int pin);              // returns 0/1
+int gpio_write(int pin, int value);   // 0 = low, 1 = high
+int gpio_read(int pin);              // returns 0 or 1
+
+#endif
